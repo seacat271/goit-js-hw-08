cad7dda3f0aa2a3731f5a197ef1f1f2ebe54ba4e
seacat271 <seacat271083@gmail.com>
2022-08-11 17:45:27 +0300

    done task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframeVideoPlayer = document.querySelector("iframe")
+const videoPlayer = new Player(iframeVideoPlayer);
+
+function getTimeData (data) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds)) 
+}
+
+videoPlayer.on('timeupdate', throttle(getTimeData, 1000));
+videoPlayer.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
