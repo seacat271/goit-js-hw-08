780f42fb06cc0e9546b86a3abd1bcd1bb1686f34
seacat271 <seacat271083@gmail.com>
2022-08-11 20:03:03 +0300

    done task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+const throttle = require('lodash.throttle');
 
+const refs = {
+   form:  document.querySelector(".feedback-form"),
+   email: document.querySelector("[name='email']"),
+   message: document.querySelector("[name='message']"),
+   submitBtn: document.querySelector("[type='submit']"),
+}
+
+
+function firstLoadPage () {
+    if(localStorage.getItem("inputFormData")) {
+        
+       const {email = "", message = "",} = JSON.parse(localStorage.getItem("inputFormData"));
+        refs.message.value = message
+        refs.email.value = email;
+    }
+}
+
+
+
+firstLoadPage ()
+
+const formValueData = {};
+
+refs.form.addEventListener("input", throttle((event) => {
+    console.log(event.target.name)
+    formValueData[event.target.name] = event.target.value;
+    localStorage.setItem("inputFormData", JSON.stringify(formValueData))
+}, 500))
+
+refs.form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    event.target.reset();
+    localStorage.removeItem("inputFormData");
+})
+
+
+
+
+console.log(JSON.parse(localStorage.getItem("inputFormData")))
