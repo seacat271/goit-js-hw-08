8da80039cf1ef50d822939fadf7b952f0450e6b3
seacat271 <seacat271083@gmail.com>
2022-08-12 15:54:34 +0300

    Fixed task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,39 +3,33 @@ const throttle = require('lodash.throttle');
 const refs = {
    form:  document.querySelector(".feedback-form"),
    email: document.querySelector("[name='email']"),
-   message: document.querySelector("[name='message']"),
-   submitBtn: document.querySelector("[type='submit']"),
+   message: document.querySelector("[name='message']")
 }
 
+let formValueData = {};
 
 function firstLoadPage () {
     if(localStorage.getItem("inputFormData")) {
-        
-       const {email = "", message = "",} = JSON.parse(localStorage.getItem("inputFormData"));
-        refs.message.value = message
+      formValueData = JSON.parse(localStorage.getItem("inputFormData"));
+      let {email = "", message = ""} = formValueData;
+        refs.message.value = message;
         refs.email.value = email;
     }
 }
 
-
-
-firstLoadPage ()
-
-const formValueData = {};
-
-refs.form.addEventListener("input", throttle((event) => {
-    console.log(event.target.name)
-    formValueData[event.target.name] = event.target.value;
-    localStorage.setItem("inputFormData", JSON.stringify(formValueData))
-}, 500))
-
-refs.form.addEventListener("submit", (event) => {
+function onClickSubmitBtn (event) {
     event.preventDefault();
     event.target.reset();
     localStorage.removeItem("inputFormData");
-})
+}
 
+function inputFormValue (event) {
+    formValueData[event.target.name] = event.target.value;
+    localStorage.setItem("inputFormData", JSON.stringify(formValueData))
+}
 
+firstLoadPage ()
 
+refs.form.addEventListener("submit", onClickSubmitBtn)
 
-console.log(JSON.parse(localStorage.getItem("inputFormData")))
+refs.form.addEventListener("input", throttle(inputFormValue, 500))
