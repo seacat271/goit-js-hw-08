c4e207b38a4457e0cc8faec4e04896b970808794
seacat271 <seacat271083@gmail.com>
2022-08-13 10:49:41 +0300

    Fixed task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,5 +8,12 @@ function getTimeData (data) {
     localStorage.setItem("videoplayer-current-time", JSON.stringify(data.seconds)) 
 }
 
+function firstLoadPage () {
+    if (!localStorage.getItem("videoplayer-current-time")) return;
+    videoPlayer.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+}
+
+firstLoadPage()
+
 videoPlayer.on('timeupdate', throttle(getTimeData, 1000));
-videoPlayer.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time")))
+
